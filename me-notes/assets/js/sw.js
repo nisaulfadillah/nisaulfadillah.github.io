ba6dc02418642748f673571e5f5dffb2e13da111
Nisaul Fadillah <nisaulfadillah5@gmail.com>
2022-02-23 09:36:13 +0700

    add fa-solid-900.woff2 to caches

--- me-notes/assets/js/sw.js
+++ me-notes/assets/js/sw.js
@@ -7,7 +7,8 @@ self.addEventListener('install', e => {
         '/me-notes/index.html',
         '/me-notes/assets/vendors/tailwind/tailwind.js',
         '/me-notes/assets/js/main.js',
-        '/me-notes/assets/vendors/fontawesome/css/all.min.css'
+        '/me-notes/assets/vendors/fontawesome/css/all.min.css',
+        '/me-notes/assets/vendors/fontawesome/webfonts/fa-solid-900.woff2'
       ]);
     })
   );
