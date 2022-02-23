fcab4139c50ef9b76703c01cd5f795bc7544d5ac
Nisaul Fadillah <nisaulfadillah5@gmail.com>
2022-02-23 10:58:34 +0700

    .

--- me-notes/assets/js/sw.js
+++ me-notes/assets/js/sw.js
@@ -3,12 +3,12 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('me-notes').then(cache => {
       return cache.addAll([
-        '/me-notes/',
-        '/me-notes/index.html',
-        '/me-notes/assets/vendors/tailwind/tailwind.js',
-        '/me-notes/assets/js/main.js',
-        '/me-notes/assets/vendors/fontawesome/css/all.min.css',
-        '/me-notes/assets/vendors/fontawesome/webfonts/fa-solid-900.woff2'
+        '/',
+        'index.html',
+        'assets/vendors/tailwind/tailwind.js',
+        'assets/js/main.js',
+        'assets/vendors/fontawesome/css/all.min.css',
+        'assets/vendors/fontawesome/webfonts/fa-solid-900.woff2'
       ]);
     })
   );
