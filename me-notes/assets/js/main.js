030178075d7e94e1f014b695ffa9bd60201ac910
Nisaul Fadillah <nisaulfadillah5@gmail.com>
2022-02-23 10:55:34 +0700

    .

--- me-notes/assets/js/main.js
+++ me-notes/assets/js/main.js
@@ -389,8 +389,6 @@ function deleteNote() {
 // register service worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/me-notes/assets/js/sw.js')
-           .then(() => {
-             console.log('Service Worker Registered');
-           });
+           .register('assets/js/sw.js')
+           .then(() => console.log('Service worker registered'));
 }
