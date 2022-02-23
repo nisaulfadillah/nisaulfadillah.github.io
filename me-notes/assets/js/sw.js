a411faa3e444d52aaf071d8a499200aa58f6e0af
Nisaul Fadillah <nisaulfadillah5@gmail.com>
2022-02-23 09:57:15 +0700

    .

--- me-notes/assets/js/sw.js
+++ me-notes/assets/js/sw.js
@@ -17,9 +17,8 @@ self.addEventListener('install', e => {
 // responding further request
 self.addEventListener('fetch', e => {
   console.log(e.request.url);
-
   e.respondWith(
-    caches.match(e.request).then(response => {    
+    caches.match(e.request).then(response => {
       return response || fetch(e.request);
     })
   );
